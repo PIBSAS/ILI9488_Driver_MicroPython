fd24e693c1f145ad6b9ac2f0ff4fe1ddba84572a
Luciano's tech <38385411+PIBSAS@users.noreply.github.com>
2026-06-07 23:05:05 -0300

    Add files via upload

---
+++ boards/Waveshare ESP32-S3 Zero/set_display.py
@@ -0,0 +1,38 @@
+from machine import SPI, Pin
+from ili9488 import driver
+
+SPI_ID = 1
+SPI_BAUDRATE = 40000000
+
+PIN_SCK  = 12
+PIN_MOSI = 11
+PIN_MISO = 13 #Only for touch board
+
+PIN_CS   = 10
+PIN_DC   = 6
+PIN_RST  = 7
+# Backlight (opcional if not 3V3 Pin available)
+PIN_BL   = 5
+
+def setting(rotation=0):
+    spi = SPI(
+        SPI_ID,
+        baudrate=SPI_BAUDRATE,
+        polarity=0,
+        phase=0,
+        sck=Pin(PIN_SCK),
+        mosi=Pin(PIN_MOSI),
+        miso=Pin(PIN_MISO)
+    )
+    Pin(PIN_BL, Pin.OUT, value=1)
+    
+    display = driver(
+        spi,
+        PIN_CS,
+        PIN_DC,
+        PIN_RST
+    )
+    
+    display.set_rotation(rotation)
+    
+    return display
