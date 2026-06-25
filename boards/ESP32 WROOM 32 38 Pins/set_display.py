46d0c0741ba467b2ab6329ddfc8d74ff96836bcc
Luciano's tech <38385411+PIBSAS@users.noreply.github.com>
2026-06-25 14:29:29 -0300

    Update set_display.py

--- boards/ESP32 WROOM 32 38 Pins/set_display.py
+++ boards/ESP32 WROOM 32 38 Pins/set_display.py
@@ -1,17 +1,26 @@
-from machine import SPI, Pin
+from machine import SPI, Pin, SDCard
 from ili9488 import driver
+import os
 
 SPI_ID = 2
 SPI_BAUDRATE = 40000000
 
-PIN_SCK  = 18
-PIN_MOSI = 23
-PIN_MISO = 19
-PIN_CS   = 5
+PIN_CS   = 5  # VSPICS0
+PIN_MOSI = 23 # VSPID
+PIN_SCK  = 18 # VSPICLK
+PIN_MISO = 19 # VSPIQ
 
-PIN_DC   = 12
-PIN_RST  = 14
+PIN_DC   = 25
+PIN_RST  = 26
 PIN_BL   = 27 # Backlight (opcional if not 3V3 Pin available)
+
+# SD Pins
+SD_CS   = 15 # HSPICS0
+SD_MOSI = 13 # HSPID
+SD_MISO = 12 # HSPIQ
+SD_SCK  = 14 # HSPICLK
+
+MOUNT_POINT = "/sd"
 
 def setting(rotation=0):
     spi = SPI(
@@ -35,3 +44,34 @@ def setting(rotation=0):
     display.set_rotation(rotation)
     
     return display
+
+def mount_sd():
+    sd = SDCard(
+        slot=2,
+        width=1,
+        sck=SD_SCK,
+        mosi=SD_MOSI,
+        miso=SD_MISO,
+        cs=SD_CS
+    )
+    
+    try:
+        os.mount(sd, MOUNT_POINT)
+    except OSError:
+        pass
+    
+    return sd
+
+
+def files():
+    try:
+        return os.listdir(MOUNT_POINT)
+    except OSError:
+        return []
+
+
+def umount():
+    try:
+        os.umount(MOUNT_POINT)
+    except OSError:
+        pass
