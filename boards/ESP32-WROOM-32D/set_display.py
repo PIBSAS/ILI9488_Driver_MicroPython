af9d9d9dd4e4e11eaca52ad6d1372f1cd37a1946
Luciano's tech <38385411+PIBSAS@users.noreply.github.com>
2026-06-25 13:54:45 -0300

    Update set_display.py

--- boards/ESP32-WROOM-32D/set_display.py
+++ boards/ESP32-WROOM-32D/set_display.py
@@ -1,5 +1,6 @@
-from machine import SPI, Pin
+from machine import SPI, Pin, SDCard
 from ili9488 import driver
+import os
 
 SPI_ID = 1
 SPI_BAUDRATE = 40000000
@@ -13,6 +14,14 @@ PIN_DC   = 16
 PIN_RST  = 17
 # Backlight (opcional if not 3V3 Pin available)
 PIN_BL   = 4
+
+# SD Pins
+SD_CS   = 5
+SD_MISO = 19
+SD_MOSI = 23
+SD_SCK  = 18
+
+MOUNT_POINT = "/sd"
 
 def setting(rotation=0):
     spi = SPI(
@@ -32,7 +41,37 @@ def setting(rotation=0):
         PIN_DC,
         PIN_RST
     )
+
+def mount_sd():
+    sd = SDCard(
+        slot=2,
+        width=1,
+        sck=SD_SCK,
+        mosi=SD_MOSI,
+        miso=SD_MISO,
+        cs=SD_CS
+    )
     
+    try:
+        os.mount(sd, MOUNT_POINT)
+    except OSError:
+        pass
+    
+    return sd
+
+
+def files():
+    try:
+        return os.listdir(MOUNT_POINT)
+    except OSError:
+        return []
+
+
+def umount():
+    try:
+        os.umount(MOUNT_POINT)
+    except OSError:
+        pass
     display.set_rotation(rotation)
     
     return display
